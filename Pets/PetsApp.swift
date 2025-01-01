7f17432a31fb04d8eb1830d07491870c1c78be69
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-01-01 14:54:02 +0200

    Initial Commit

---
+++ Pets/PetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsApp.swift
+//  Pets
+//
+//  Created by Tony on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetsTests/PetsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetsTests.swift
+//  PetsTests
+//
+//  Created by Tony on 1/1/25.
+//
+
+import Testing
+@testable import Pets
+
+struct PetsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
